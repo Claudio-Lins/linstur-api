6ff5d11fcbfc6a618bc6c3d94bd956caca004a51
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-01-21 08:57:17 +0000

    add app.get...

--- app.js
+++ app.js
@@ -16,12 +16,25 @@ mongoose.connect('mongodb+srv://clins:Deca0147@cluster0.zwsrt.mongodb.net/test',
   console.log('Error: Conexão com MongoDB não realizada ' + erro)
 })
 
-app.use("/kms", (req, res) => {
+app.get("/kms", (req, res) => {
   return res.json({
     error: false,
     mensage: "Info Pg Kms",
   });
 });
+
+app.post("/kms", (req, res) => {
+  Kms.create(req.body, (err) => {
+    if(err) return res.status(400).json({
+      error:true,
+      mensage: 'Erro: Conteudo da página KMs não cadastrado com Sucesso!'
+    })
+  })
+  return res.json({
+    error: false,
+    mensage: 'Erro: Conteudo da página KMs cadastrado com Sucesso!'
+  })
+})
 
 app.listen(8080, () => {
   console.log(
