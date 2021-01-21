f3d3f405c8882fcaaf038418acf49a43d221d2e0
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-01-21 08:39:09 +0000

    Conect MongoDB

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-app.use("/home", (req, res) => {
+require('./models/kms')
+const Kms = mongoose.model('Kms')
+
+
+
+mongoose.connect('mongodb+srv://clins:Deca0147@cluster0.zwsrt.mongodb.net/test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Conexão com MongoDB realizada com sucesso')
+}).catch((erro) => {
+  console.log('Error: Conexão com MongoDB não realizada ' + erro)
+})
+
+app.use("/kms", (req, res) => {
   return res.json({
     error: false,
-    mensage: "Info Pg Home",
+    mensage: "Info Pg Kms",
   });
 });
 
@@ -13,3 +28,5 @@ app.listen(8080, () => {
     'Servidor Iniciado na porta 8080: http://localhost"8080 com Sucesso!'
   );
 });
+
+//mongodb+srv://clins:Deca0147@cluster0.zwsrt.mongodb.net/test'
